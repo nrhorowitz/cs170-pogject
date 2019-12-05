3fff7620250c5c9200f58eede138a279495329dc
varunrmurthy <varun_murthy@berkeley.edu>
2019-12-05 14:29:54 -0800

    comments added to simulated Annealing

--- algorithm/simAnn.py
+++ algorithm/simAnn.py
@@ -1,20 +1,26 @@
 from random import randint
+#an object represents each vertex in original graph
 class Point:
     """A simple example class"""
     def __init__(self, label, adjacentVertices):
         self.label = label
-        #list of points
         self.adjacentVertices = adjacentVertices
+#an object representing a point in the path
 class pathPoint:
     def __init__(point, dropoffs):
+        #int labeling graph vertices from 0 to |V| - 1
         self.label = point.label
+        #list of vertices that this vertex has an edge to in the original graph
         self.adjacentVertices = point.adjacentVertices
+        #list of destinations for people dropped off here
         self.dropOffs = dropOffs
 
-        #path is list of points in order of the path. points is a list of points that repres
-    def simulatedAnnealing(path, points):
-        choice = randint(0, 10)
-        #add a random vertex
+#path is list of pathPoints in order of the path. points is a list of points that represent the graph
+def simulatedAnnealing(path, points, iters):
+    choice = randint(0, 10)
+    #add a random vertex
+    for _ in range(iters):
+        currPath = path
         if (choice >= 7):
             pointToAdd = points[randint(0, len(points) - 1)]
             pointToAdd = pathPoint(pointToAdd, [])
@@ -27,10 +33,17 @@ class pathPoint:
             place = randint(1, len(path) - 2)
             tempPoint = path[place]
             if (path[place - 1].label in path[place + 1].adjacentVertices):
+                if(tempPoint.dropOffs):
+                    for i in tempPoint.dropOffs:
+                        destination = randInt[0,1]
+                        if destination:
+                            path[place + 1].dropoffs.append(i)
+                        else:
+                            path[place - 1].dropoffs.append(i)
                 path = path[:place] + path[place + 1:]
-            if(tempPoint.dropOffs):
-                for i in tempPoint.dropOffs
         else:
+
+
 
 
 
