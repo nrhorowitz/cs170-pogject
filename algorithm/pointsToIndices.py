9292f25c33c59e26071f4f679c13c47c967bc0e6
Owen Gong <goldenowen@berkeley.edu>
2019-12-05 15:36:17 -0800

    path_points to car_cycle and dropoff_points

--- algorithm/pointsToIndices.py
+++ algorithm/pointsToIndices.py
@@ -1,10 +1,15 @@
 # Input: List of pathPoints, List of locations
 # Output: List of indices of car_cycle and list of indices of droppoff points
 
-def pointToIndices(path_points, list_of_locations):
+def pointToIndices(path_points):
     car_cycle = []
     droppoff_points = {} # maps dropoff location index to a list of index of TA homes
-
+    for point in path_points:
+        car_cycle.append(point.label)
+        homes_list = []
+        for index in point.dropOffs:
+            home_list.append(index)
+        dropoff_points[point.label] = home_list
     return car_cycle, dropoff_points
 
 
