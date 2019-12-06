20defb23d7d62e86b5f05f509652f83fff341440
Nathan Horowitz <nhorowitz@berkeley.edu>
2019-12-05 17:32:56 -0800

    main skeleton

--- algorithm/main.py
+++ algorithm/main.py
@@ -5,12 +5,12 @@ MAX_VALUE = float('inf')
 
 # Given file name, return input in data form
 # convert input to null path and list of points and distance lookup
+# returns list of points, global lookup for distance between points
 def read_input(file_name):
     f = open("inputs/" + file_name, "r")
     contents = f.readlines()
     labelLookup = contents[2].split(" ")
     labelLookup[len(labelLookup) - 1] = labelLookup[len(labelLookup) - 1][:len(labelLookup[len(labelLookup) - 1]) - 1]
-    # print(labelLookup)
     listOfPoints = []
     globalLookup = []
     for i in range(5, 5 + int(contents[0])):
@@ -26,32 +26,22 @@ def read_input(file_name):
                 adjacentList.append(j)
             else:
                 globalLookupRow.append(MAX_VALUE)
-        print(adjacentList)
         globalLookup.append(globalLookupRow)
         p = Point(i - 5, adjacentList, labelLookup[i - 5])
         listOfPoints.append(p)
-    print(globalLookup)
-    
+    return listOfPoints, globalLookup
+
+def run_solver():
     return 0
 
-# Given table, return shortest path and distance between any two vertices
-def shortest_path_any_two(input):
+# Given list of path points, generate output file with name
+def generate_output(listOfPathPoints, name):
     return 0
 
-# Return total energy expended, if TA cannot get home return float('inf')
-def path_cost(input, path):
-    return 0
-
-# Add a random vertex to a random part of the path
-# TODO: probably should be close by
-def add_vertex(input, path):
-    return 0
-
-# Remove a random vertex from the path
-def remove_vertex(input, path):
-    return 0
-
-# Dropoff point 
-def change_dropoff(input, path):
-    c = 3
+def sweep_inputs():
+    # try every input, 
+    # read_input ---> list of points, global distance lookup
+    # start with some guess ****** pathpoint
+    # run_solver on list of pathpoints ---> the correct list of pathpoints
+    # generate_output correct list of pathpoint ---> .out
     return 0
